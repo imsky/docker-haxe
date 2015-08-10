1a8d46d2e833811cd8c85f85c101a991a9f675f4
Ivan Malopinsky <ivan@imsky.co>
2015-08-10 00:05:49 -0400

    add verify script

---
+++ test/verify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+build/cpp/Test          && echo '> C++ passed'
+node build/test.js      && echo '> JavaScript passed'
+python3 build/test.py   && echo '> Python passed'
+php build/php/index.php && echo '> PHP passed'
+
+ls -la
